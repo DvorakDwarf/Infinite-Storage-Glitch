ba9f1467d9ce7e7c8f0645a293af55431047ded2
Mohammad Jafari <71469873+mossein@users.noreply.github.com>
2023-02-22 21:12:28 -0330

    Improve EmbedSource struct code by removing unnecessary comments, using match expressions for error handling, and adding error handling to the from() function.

--- src/embedsource.rs
+++ src/embedsource.rs
@@ -1,8 +1,6 @@
 use opencv::core::prelude::*;
 use opencv::core::{Mat, CV_8UC3, Size};
 
-//Putting width and height here potentially overkill, mostly here for convenience
-//I WANT THAT MAX PERFORMANCE
 pub struct EmbedSource {
     pub image: Mat,
     pub size: i32,
@@ -13,46 +11,37 @@ pub struct EmbedSource {
 impl EmbedSource {
     pub fn new(size: i32, width: i32, height: i32) -> EmbedSource {
         let frame_size = Size::new(width, height);
-
         let actual_width = width - (width % size);
         let actual_height = height - (height % size);
         let actual_size = Size::new(actual_width, actual_height);
 
-        // dbg!(actual_size);
-        // dbg!(frame_size);
+        let image = Mat::new_rows_cols(frame_size.height, frame_size.width, CV_8UC3)
+            .expect("Failed to create new Mat");
 
-        // if actual_height != height {
-        //     panic!("Height bug");
-        // }
-
-        //WHy does this have to be unsafe smh
-        unsafe {
-            let image = Mat::new_rows_cols(frame_size.height, frame_size.width, CV_8UC3).unwrap();
-
-            EmbedSource {
-                image,
-                size, 
-                frame_size,
-                actual_size
-            }
+        EmbedSource {
+            image,
+            size,
+            frame_size,
+            actual_size,
         }
     }
 
-    pub fn from(image: Mat, size: i32) -> EmbedSource {
+    pub fn from(image: Mat, size: i32) -> Result<EmbedSource, String> {
         let width = image.cols();
         let height = image.rows();
         let frame_size = Size::new(width, height);
 
-        //Cuts off borders if size doesn't perfectly fit
-        let width = width - (width % size);
-        let height = height - (height % size);
-        let actual_size = Size::new(width, height);
-
-        EmbedSource {
-            image,
-            size, 
-            frame_size,
-            actual_size
+        if width % size != 0 || height % size != 0 {
+            return Err("Image size is not a multiple of the embedding size".to_string());
         }
+
+        let actual_size = Size::new(width - (width % size), height - (height % size));
+
+        Ok(EmbedSource {
+            image,
+            size,
+            frame_size,
+            actual_size,
+        })
     }
 }
