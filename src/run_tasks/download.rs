9d69a07a9ff5ac59f223739badd6bb36c84b3380
Sebastian Beyer <sebastian.beyer@live.com>
2023-02-27 00:32:48 +0100

    Fix issue 62

--- src/run_tasks/download.rs
+++ src/run_tasks/download.rs
@@ -1,22 +1,42 @@
-use youtube_dl::{download_yt_dlp, YoutubeDl};
+use youtube_dl::{download_yt_dlp};
+use std::process::Command;
 
 use crate::args::DownloadParams;
 
 pub async fn run_download(args: DownloadParams) -> anyhow::Result<()> {
     let yt_dlp_path = download_yt_dlp(".").await?;
+    
+    let url = args.url.expect("No URL in params when run_download");
+    
+    // check if the yt_dlp_path exists
+    if !yt_dlp_path.exists() {
+        println!("yt-dlp not found");
+        return Ok(());
+    }
+    
+    // Output path for the video has the format: `downloaded_{timestamp}.mp4`
+    let timestamp = chrono::Local::now().format("%Y-%m-%d_%H-%M-%S");
+    let download_path = format!("downloaded_{}.mp4", timestamp);
 
+    // Use the yt-dlp binary to download the video: `yt-dlp -f mp4 -o video.mp4 {url}`
     println!("Starting the download, there is no progress bar");
-    let output = YoutubeDl::new(&args.url.expect("No URL in params when run_download"))
-        .youtube_dl_path(yt_dlp_path)
-        .format("best")
-        .download(true)
-        .run_async()
-        .await?;
+    let output = Command::new(yt_dlp_path)
+        .arg("-f")  // format
+        .arg("mp4") // mp4
+        .arg("-o")  // output
+        .arg(download_path.clone()) // output path
+        .arg(url)  // url to download from
+        .output()
+        .expect("Failed to execute command");
 
-    let _video = output.into_single_video().unwrap();
-    // let title = video.title;
-
-    println!("Video downloaded successfully");
+    // check the output of the command
+    if output.status.success() {
+        println!("Video downloaded successfully");
+        println!("Output path: {}", std::fs::canonicalize(download_path).unwrap().display());
+    } else {
+        println!("Video download failed");
+        println!("Error: {}", String::from_utf8_lossy(&output.stderr));
+    }
 
     return Ok(());
 }
